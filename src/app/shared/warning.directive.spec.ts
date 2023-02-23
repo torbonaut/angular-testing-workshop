049a477f49ac4162e01b62a70e3c18a889ed0bc4
Torben Gallob <office@lalit.at>
2023-02-24 00:13:08 +0100

    added tests for directive

--- src/app/shared/warning.directive.spec.ts
+++ src/app/shared/warning.directive.spec.ts
@@ -1,8 +1,42 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 import { WarningDirective } from './warning.directive';
 
+@Component({
+  template: ` <div appWarning></div> `,
+})
+class TestComponent {}
+
 describe('WarningDirective', () => {
-  it('should create an instance', () => {
-    const directive = new WarningDirective();
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestComponent, WarningDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should change class on mouseover', () => {
+    const div = fixture.nativeElement.querySelector('div');
+    div.dispatchEvent(new MouseEvent('mouseover'));
+    fixture.detectChanges();
+    expect(div.classList).toContain('bg-slate-100');
+  });
+
+  it('should change class back on mouseout', () => {
+    const div = fixture.nativeElement.querySelector('div');
+    div.dispatchEvent(new MouseEvent('mouseover'));
+    fixture.detectChanges();
+    expect(div.classList).toContain('bg-slate-100');
+    div.dispatchEvent(new MouseEvent('mouseout'));
+    fixture.detectChanges();
+    expect(div.classList).toContain('bg-yellow-50');
   });
 });
